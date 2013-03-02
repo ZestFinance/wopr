e910618bb39b473f06a1ad4433806e330d84d7dc
Alexander Tamoykin <at@zestcash.com>
2013-03-01 22:52:42 -0800

    Wopr.configure reads from config/wopr.yml

--- lib/wopr.rb
+++ lib/wopr.rb
@@ -1,4 +1,6 @@
 require "wopr/version"
+require "yaml"
+require "rainbow"
 
 module Wopr
   class WoprError < StandardError; end
@@ -6,11 +8,20 @@ module Wopr
   class TimeoutError < WoprError; end
 
   class << self
-    attr_accessor :twilio_account_sid, :twilio_auth_token, :port, :twilio_callback_host, :twilio_server_port,
+    attr_accessor :twilio_account_sid,
+      :twilio_auth_token,
+      :twilio_callback_host,
+      :twilio_server_port,
       :default_wait_time
 
-    def configure
-      yield self
+    def configure config_file = File.join(File.dirname(__FILE__), '..', 'config', 'wopr.yml')
+      check_config config_file
+
+      config = YAML.load_file config_file
+      check_for_twilio_creds_in config
+
+      @default_wait_time = config['default_wait_time'] || 20
+      @twilio_server_port = config['twilio_server_port'] || 4500
     end
 
     def boot
@@ -36,6 +47,29 @@ module Wopr
 
     private
 
+    def check_config config_file
+      unless File.exists? File.expand_path(config_file)
+        complain
+        exit 1
+      end
+    end
+
+    def complain
+      puts "---"
+      puts "Gash! wopr is not configured".color(:red)
+      puts "Open config/wopr.yml and follow the instructions"
+    end
+
+    def check_for_twilio_creds_in config
+      if config["twilio_account_sid"].empty? || config["twilio_auth_token"].empty?
+        puts "Dude, you need to configure twilio_account_sid and twilio_auth_token in config/wopr.yml".color(:red)
+        exit 1
+      else
+        @twilio_account_sid = config["twilio_account_sid"]
+        @twilio_auth_token  = config["twilio_auth_token"]
+      end
+    end
+
     def find_available_port
       server = TCPServer.new('127.0.0.1', 0)
       server.addr[1]
@@ -50,8 +84,4 @@ module Wopr
   autoload :TwilioService, 'wopr/twilio_service'
   autoload :Bot, 'wopr/bot'
   autoload :DSL, 'wopr/dsl'
-end
-
-Wopr.configure do |configure|
-  configure.default_wait_time = 20
 end
--- spec/wopr_spec.rb
+++ spec/wopr_spec.rb
@@ -1,14 +1,54 @@
 require 'spec_helper'
 
 describe Wopr do
-  describe 'passing a block to .configure' do
-    it 'allows the block to set attributes on wopr' do
-      Wopr.configure do |config|
-        config.twilio_account_sid = 'foo'
-        config.twilio_auth_token = 'bar'
+  describe ".configure" do
+    context "given that configuration file exists" do
+      before { File.stub(:exists?).and_return(true) }
+
+      context "and there are twilio credentials" do
+        before do
+          @config = {
+            "default_wait_time"   => 50,
+            "twilio_server_port"  => 4501,
+            "twilio_account_sid"  => "abc",
+            "twilio_auth_token"   => "xyz"
+          }
+          YAML.stub(:load_file).and_return(@config)
+        end
+
+        it "sets twilio creds" do
+          Wopr.configure
+          Wopr.twilio_account_sid.should == "abc"
+          Wopr.twilio_auth_token.should == "xyz"
+        end
+
+        it "configures the rest" do
+          Wopr.configure
+          Wopr.default_wait_time.should  == 50
+          Wopr.twilio_server_port.should == 4501
+        end
       end
-      Wopr.twilio_account_sid.should == 'foo'
-      Wopr.twilio_auth_token = 'bar'
+
+      context "and there is no twilio credentials" do
+        before do
+          @config = { "twilio_account_sid"  => "" }
+          YAML.stub(:load_file).and_return(@config)
+        end
+
+        it "complains about twilio and exits" do
+          Wopr.should_receive(:puts).with(/you need to configure twilio/)
+          expect { Wopr.configure }.to raise_error SystemExit
+        end
+      end
+    end
+
+    context "given there is no configuration file" do
+      before { File.stub(:exists?).and_return(false) }
+
+      it "fails and complains" do
+        Wopr.should_receive(:complain)
+        expect { Wopr.configure }.to raise_error
+      end
     end
   end
 
