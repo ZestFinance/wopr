b073d627f24012790e5716acdffbf11af294421d
Alexander Tamoykin <at@zestcash.com>
2013-03-02 17:34:44 -0800

    Create bots in Wopr.configure

--- lib/wopr.rb
+++ lib/wopr.rb
@@ -8,29 +8,29 @@ module Wopr
   class TimeoutError < WoprError; end
 
   class << self
+    CONFIG_PATH = File.join(File.dirname(__FILE__), '..', 'config', 'wopr.yml')
+
     attr_accessor :twilio_account_sid,
       :twilio_auth_token,
       :twilio_callback_host,
       :twilio_server_port,
       :default_wait_time
 
-    def configure config_file = File.join(File.dirname(__FILE__), '..', 'config', 'wopr.yml')
-      check_config config_file
+    def configure config_file = CONFIG_PATH
+      check_config_existence  config_file
 
       config = YAML.load_file config_file
-      check_for_twilio_creds_in config
 
-      @default_wait_time = config['default_wait_time'] || 20
+      check_twilio config
+      create_bots  config
+
+      @default_wait_time  = config['default_wait_time']  || 20
       @twilio_server_port = config['twilio_server_port'] || 4500
     end
 
     def boot
       TwilioService.new.update_callbacks(Wopr::Bot.all.map(&:phone_number))
       TwilioCallbackServer.boot
-    end
-
-    def twilio_callback_host
-      @twilio_callback_host ||= LocalTunnelHost.acquire(twilio_server_port)
     end
 
     def twilio_server_port
@@ -47,7 +47,23 @@ module Wopr
 
     private
 
-    def check_config config_file
+    def create_bots config
+      config["bots"].each do |name, params|
+        Wopr::Bot.create name, params
+      end if config["bots"]
+    end
+
+    def check_twilio config
+      if config["twilio_account_sid"].empty? || config["twilio_auth_token"].empty?
+        puts "Dude, you need to configure twilio_account_sid and twilio_auth_token in config/wopr.yml".color(:red)
+        exit 1
+      else
+        @twilio_account_sid = config["twilio_account_sid"]
+        @twilio_auth_token  = config["twilio_auth_token"]
+      end
+    end
+
+    def check_config_existence config_file
       unless File.exists? File.expand_path(config_file)
         complain
         exit 1
@@ -58,16 +74,6 @@ module Wopr
       puts "---"
       puts "Gash! wopr is not configured".color(:red)
       puts "Open config/wopr.yml and follow the instructions"
-    end
-
-    def check_for_twilio_creds_in config
-      if config["twilio_account_sid"].empty? || config["twilio_auth_token"].empty?
-        puts "Dude, you need to configure twilio_account_sid and twilio_auth_token in config/wopr.yml".color(:red)
-        exit 1
-      else
-        @twilio_account_sid = config["twilio_account_sid"]
-        @twilio_auth_token  = config["twilio_auth_token"]
-      end
     end
 
     def find_available_port
--- spec/wopr_spec.rb
+++ spec/wopr_spec.rb
@@ -1,6 +1,26 @@
 require 'spec_helper'
 
 describe Wopr do
+  describe ".boot" do
+    before do
+      Wopr.twilio_account_sid = 'fake_sid'
+      Wopr.twilio_auth_token  = 'auth_token'
+      @twilio_service = Wopr::TwilioService.new
+      Wopr::TwilioService.stub(:new).and_return(@twilio_service)
+      @twilio_service.stub :update_callbacks
+    end
+
+    it "updates twilio callbacks" do
+      @twilio_service.should_receive :update_callbacks
+      Wopr.boot
+    end
+
+    it "boots the TwilioCallbackServer" do
+      Wopr::TwilioCallbackServer.should_receive :boot
+      Wopr.boot
+    end
+  end
+
   describe ".configure" do
     context "given that configuration file exists" do
       before { File.stub(:exists?).and_return(true) }
@@ -40,6 +60,29 @@ describe Wopr do
           expect { Wopr.configure }.to raise_error SystemExit
         end
       end
+
+      context "and there is a bot" do
+        before do
+          @config = {
+            "bots" => {
+              "customer" => {
+                "id" => 0,
+                "phone_number" => 123
+              }
+            }
+          }
+          YAML.stub(:load_file).and_return(@config)
+          Wopr.stub(:check_twilio)
+        end
+
+        it "creates a customer bot" do
+          Wopr.configure
+
+          customer = Wopr::Bot[:customer]
+          customer.name.should == "customer"
+          customer.phone_number.should == 123
+        end
+      end
     end
 
     context "given there is no configuration file" do
@@ -49,25 +92,6 @@ describe Wopr do
         Wopr.should_receive(:complain)
         expect { Wopr.configure }.to raise_error
       end
-    end
-  end
-
-  describe ".boot" do
-    before do
-      Wopr.twilio_account_sid = 'fake_sid'
-      Wopr.twilio_auth_token  = 'auth_token'
-      @twilio_service = Wopr::TwilioService.new
-      Wopr::TwilioService.stub(:new).and_return(@twilio_service)
-    end
-
-    it "updates twilio callbacks" do
-      @twilio_service.should_receive :update_callbacks
-      Wopr.boot
-    end
-
-    it "boots the TwilioCallbackServer" do
-      Wopr::TwilioCallbackServer.should_receive :boot
-      Wopr.boot
     end
   end
 end
